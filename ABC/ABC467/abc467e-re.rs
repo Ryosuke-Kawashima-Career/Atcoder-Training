7462cdb36de28750cb92f8eb14a849b511be21f3
Ryosuke-Kawashima-Career <ryosuke.kawashima.career@gmail.com>
2026-07-21 20:37:26 +0530

    ABC467E has been solved, but I do not fully understand the algorithm yet...

--- ABC/ABC467/abc467e-re.rs
+++ ABC/ABC467/abc467e-re.rs
@@ -3,21 +3,21 @@ use std::collections::BTreeMap;
 fn main() {
     input! {n: usize, m: i64, a: [i64; n], b: [i64; n-1]}
     // Step1: Calculate Difference
-    let mut sum_a: Vec<i64> = vec![0; n-1];
-    let mut diff: Vec<i64> = vec![0; n-1];
-    for i in 0..n-1 {
-        sum_a[i] = (a[i] + a[i+1]) % m;
-        diff[i] = (b[i] + m - sum_a[i]) % m;     
+    let mut sum_a: Vec<i64> = vec![0; n - 1];
+    let mut diff: Vec<i64> = vec![0; n - 1];
+    for i in 0..n - 1 {
+        sum_a[i] = (a[i] + a[i + 1]) % m;
+        diff[i] = (b[i] + m - sum_a[i]) % m;
     }
 
     // Step2: Estimate Base Increment
     let mut base: Vec<i64> = vec![0; n];
     for i in 1..n {
-        base[i] = (diff[i-1] + m - base[i-1]) % m;
+        base[i] = (diff[i - 1] + m - base[i - 1]) % m;
     }
     // Step3: Slope of parities
     let mut n_odd: usize = 0;
-    let mut n_even: usize = 0
+    let mut n_even: usize = 0;
     for i in 0..n {
         if i % 2 == 0 {
             n_even += 1;
@@ -40,4 +40,24 @@ fn main() {
     }
 
     // Step5: Sweep line algorithm
+    let mut init_val: i64 = 0;
+    for i in 0..n {
+        init_val += base[i];
+    }
+
+    let mut min_val: i64 = init_val;
+    let mut curr_val: i64 = init_val;
+    let mut curr_v: i64 = 0;
+    for (&next_v, &delta) in delta_map.iter() {
+        if next_v >= m {
+            continue;
+        }
+        let steps: i64 = next_v - 1 - curr_v;
+        let val_before: i64 = curr_val + slope as i64 * steps;
+        let val_at: i64 = val_before + slope as i64 + delta;
+        min_val = min_val.min(val_at);
+        curr_val = val_at;
+        curr_v = next_v;
+    }
+    println!("{}", min_val);
 }
